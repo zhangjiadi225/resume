03a15982b7bf9c2f4f27669543848f303f8054c9
zhang11ca <7674207+zhang11ca@user.noreply.gitee.com>
2022-05-01 20:02:07 +0800

    首页结束

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,25 +1,53 @@
 <script setup>
-import { reactive } from 'vue';
-const arr = reactive(['你不一定要很厲害，才能開始','你要開始，才能很厲害'])
+import { reactive } from 'vue'
+const arr = reactive(['你不一定要很厲害，才能開始', '你要開始，才能很厲害'])
 </script>
 
 <template>
   <div>
     <div class="title">兴趣使然的前端开发</div>
     <Describe v-for="item in arr">
-      {{item}}
+      {{ item }}
     </Describe>
+    <div class="show">
+      <iframe
+        src="http://127.0.0.1:5500/public/katong.html"
+        style="width: 100%; height: 100%"
+        scrolling="auto"
+        frameborder="0"
+      >
+      </iframe>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.title{
+.title {
   position: relative;
   font-size: 28px;
   font-weight: 700;
   left: 340px;
-  background: linear-gradient(to right top, #69aeea, #7998e0, #8f7fce, #a364b1, #af468c);
-	  -webkit-background-clip: text;
-	  color: transparent;
+  background: linear-gradient(
+    to right top,
+    #69aeea,
+    #7998e0,
+    #8f7fce,
+    #a364b1,
+    #af468c
+  );
+  -webkit-background-clip: text;
+  color: transparent;
+}
+.show {
+  margin: auto;
+  margin-top: 50px;
+  width: 80%;
+  height: 240px;
+  border: 1px solid #a364b1;
+  position: relative;
+  display: flex; //开启弹性布局
+  justify-content: center;
+  align-items: center;
+  min-height: 240px; // 文字居中
 }
 </style>
